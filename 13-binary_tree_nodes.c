2c7b5d3bfd2631fd5c80e486e7659692b8068be4
hima890 <hfibrahim90@gmail.com>
2024-06-05 00:19:37 +0200

    Features/Count-nudes-2

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,20 +7,18 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t count = 0;
+
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size_t count = 0;
-
-	if (tree->left != NULL || tree->right != NULL)
+	else
 	{
-		count = 1;
+		count += ((tree->left || tree->right) ? 1 : 0);
+		count += binary_tree_nodes(tree->left);
+		count += binary_tree_nodes(tree->right);
+		return (count);
 	}
-
-	count += ((tree->left || tree->right) ? 1 : 0);
-	count += binary_tree_nodes(tree->left);
-	count += binary_tree_nodes(tree->right);
-	return (count);
 }
