b0f5568071ff5cff4fc29fe76f68f73c37135be8
Ruba-Salih <rubasalih900@gmail.com>
2024-06-04 23:03:44 +0300

    Edit task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,12 +10,13 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	size_t size = 0;
 
-	/* Recursively calculate the size of the left and right subtrees */
-	size_t left_size = binary_tree_size(tree->left);
-	size_t right_size = binary_tree_size(tree->right);
-
-	return (1 + (left_size + right_size));
+	if (tree)
+	{
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
 }
