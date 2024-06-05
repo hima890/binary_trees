d9ef24ca00c0dd0a2f0a64eecd917d7e40e6286b
Ruba-Salih <rubasalih900@gmail.com>
2024-06-05 15:51:24 +0300

    Task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -37,12 +37,12 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	while (first && second)
 	{
 		if (first == second)
-			return (first);
+			return ((binary_tree_t *)first);
 		first = first->parent;
 		second = second->parent;
 	}
 
-	return (first);
+	return ((binary_tree_t *)first);
 }
 
 /**
